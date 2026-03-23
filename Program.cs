12a1202f145ec0f8cc89f24b3cb9a68d7e9dae77
Your friendly neighborhood programmer <colombo.jordan@franchettisalviani.net>
2026-03-23 08:58:33 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿namespace matrixes_6
+{
+    internal class Program
+    {
+        static int[,] Tablet_maker()
+        {
+            int multiplier = 1,n=0;
+            int[,] matrix = new int[10, 10];
+            for(int i = matrix.GetLength(0); i > 0; i--)
+            {
+                for(int j = matrix.GetLength(1); j > 0; j--)
+                {
+                    matrix[i, j] = n * multiplier;
+                    n++;
+                }
+                multiplier++;
+                n = 1;
+            }
+            return matrix;
+
+        }
+        static void Matrix_printer(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                Console.WriteLine("");
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write($"[{matrix[i, j]}]"+'\t');
+                }
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
